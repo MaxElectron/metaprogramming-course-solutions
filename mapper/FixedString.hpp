5ce7a14375b3f8bd222167f3335635782d13f5b0
chernjatinal <sasha.chernyatin@gmail.com>
2024-12-20 23:59:35 +0300

    themendously impressive mapper code

--- mapper/FixedString.hpp
+++ mapper/FixedString.hpp
@@ -1,12 +1,25 @@
 #pragma once
 
+#include <algorithm>
+#include <array>
+#include <string_view>
 
-template<size_t max_length>
-struct FixedString {
-  FixedString(const char* string, size_t length);
-  operator std::string_view() const;
+template <size_t max_len> struct FixedString {
+  std::array<char, max_len + 1> data;
+  size_t length;
 
-  // std::string impl; ???
+  constexpr FixedString(const char *input_str, size_t input_len) noexcept
+      : data{}, length(input_len) {
+    std::ranges::copy(input_str, input_str + input_len, data.begin());
+    data[input_len] = '\0';
+  }
+
+  constexpr operator std::string_view() const noexcept {
+    return std::string_view(data.data(), length);
+  }
 };
 
-// operator ""_cstr ?
+constexpr FixedString<256> operator"" _cstr(const char *literal_str,
+                                            size_t literal_len) {
+  return FixedString<256>(literal_str, literal_len);
+}
