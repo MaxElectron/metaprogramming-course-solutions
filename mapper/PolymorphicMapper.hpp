83e40f506da439c2a2cc459d1d49d0761e49056d
Alexander Chernyatin <71085941+MaxElectron@users.noreply.github.com>
2024-12-23 00:24:50 +0300

    Billy, we need a map

--- mapper/PolymorphicMapper.hpp
+++ mapper/PolymorphicMapper.hpp
@@ -14,38 +14,45 @@ struct MapperImpl;
 
 template <class Base, class Target, class... MapEntries>
 struct PolymorphicMapper {
-  static std::optional<Target> map(const Base& instance) {
-    return MapperImpl<Base, Target, MapEntries...>::processMapping(instance);
-  }
+    static std::optional<Target> map(const Base& instance) {
+        return MapperImpl<Base, Target, MapEntries...>::processMapping(instance);
+    }
 };
 
 template <class Base, class Target, class Derived, auto mappedValue, class... RemainingMappings>
 struct MapperImpl<Base, Target, Mapping<Derived, mappedValue>, RemainingMappings...> {
-    static_assert(std::is_base_of_v<Base, Derived>);
     
-  static std::optional<Target> processMapping(const Base& instance) {
-    if constexpr (std::is_same_v<std::remove_cv_t<decltype(mappedValue)>, Target>) {
-          if (dynamic_cast<const Derived*>(std::addressof(instance)) != nullptr) {
-            return {mappedValue};
+    template<class T = Target>
+    static std::enable_if_t<std::is_same_v<T, std::remove_cv_t<decltype(mappedValue)>>, std::optional<Target>>
+    helper(const Base& instance) {
+        if (dynamic_cast<const Derived*>(std::addressof(instance)) != nullptr) {
+          return {mappedValue};
+        } else {
+          if constexpr (sizeof...(RemainingMappings) > 0) {
+            return MapperImpl<Base, Target, RemainingMappings...>::processMapping(instance);
           } else {
-            if constexpr (sizeof...(RemainingMappings) > 0) {
-              return MapperImpl<Base, Target, RemainingMappings...>::processMapping(instance);
-            } else {
-              return std::nullopt;
-            }
+            return std::nullopt;
+          }
         }
     }
-    else {
+    
+    template<class T = Target>
+    static std::enable_if_t<!std::is_same_v<T, std::remove_cv_t<decltype(mappedValue)>>, std::optional<Target>>
+    helper(const Base& instance) {
         return std::nullopt;
     }
-  }
+
+    static std::optional<Target> processMapping(const Base& instance) {
+        static_assert(std::is_base_of_v<Base, Derived>);
+        return helper(instance);
+    }
 };
 
 template <class Base, class Target>
 struct MapperImpl<Base, Target> {
-  static std::optional<Target> processMapping(const Base&) {
-    return std::nullopt;
-  }
+    static std::optional<Target> processMapping(const Base&) {
+        return std::nullopt;
+    }
 };
 
 template <class Base, class Target>
