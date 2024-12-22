64b7ea83a94eab012cc7c0a5b196ea74c7628e1c
Alexander Chernyatin <71085941+MaxElectron@users.noreply.github.com>
2024-12-23 01:45:47 +0300

    Simplify

--- mapper/PolymorphicMapper.hpp
+++ mapper/PolymorphicMapper.hpp
@@ -12,40 +12,39 @@ struct Mapping {
 template <class Base, class Target, class... MapEntries>
 struct PolymorphicMapper;
 
+template <class Base, class Target>
+struct PolymorphicMapper<Base, Target>
+{
+    static std::optional<Target> map(const Base&) { return std::nullopt; }
+};
+
 template <class Base, class Target, class Derived, auto mappedValue, class... RemainingMappings>
-struct PolymorphicMapper<Base, Target, Mapping<Derived, mappedValue>, RemainingMappings...> {
-    template<class T = Target>
-    static std::enable_if_t<std::is_same_v<T, std::remove_cv_t<decltype(mappedValue)>>, std::optional<Target>>
-    map(const Base& instance) {
+struct PolymorphicMapper<Base, Target, Mapping<Derived, mappedValue>, RemainingMappings...>
+{
+    static std::optional<Target> map(const Base& instance)
+    {
         static_assert(std::is_base_of_v<Base, Derived>);
-        if (dynamic_cast<const Derived*>(std::addressof(instance)) != nullptr) {
-            return {mappedValue};
-        } else {
-            if constexpr (sizeof...(RemainingMappings) > 0) {
-                return PolymorphicMapper<Base, Target, RemainingMappings...>::map(instance);
-            } else {
+        if (dynamic_cast<const Derived*>(std::addressof(instance)) != nullptr)
+        {
+            if constexpr (std::is_same_v<Target, std::remove_cv_t<decltype(mappedValue)>>)
+            {
+                return { mappedValue };
+            }
+            else
+            {
                 return std::nullopt;
             }
         }
-    }
-
-    template<class T = Target>
-    static std::enable_if_t<!std::is_same_v<T, std::remove_cv_t<decltype(mappedValue)>>, std::optional<Target>>
-    map(const Base& instance) {
-        static_assert(std::is_base_of_v<Base, Derived>);
-        if constexpr (sizeof...(RemainingMappings) > 0) {
-            return PolymorphicMapper<Base, Target, RemainingMappings...>::map(instance);
-        }
         else
         {
-            return std::nullopt;
+            if constexpr (sizeof...(RemainingMappings) > 0)
+            {
+                return PolymorphicMapper<Base, Target, RemainingMappings...>::map(instance);
+            }
+            else
+            {
+                return std::nullopt;
+            }
         }
-    }
-};
-
-template <class Base, class Target>
-struct PolymorphicMapper<Base, Target> {
-    static std::optional<Target> map(const Base&) {
-        return std::nullopt;
     }
 };
