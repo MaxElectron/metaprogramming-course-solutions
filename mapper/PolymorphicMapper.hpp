368215d0c97126fbde167a07165b2e1b4901877d
Alexander Chernyatin <71085941+MaxElectron@users.noreply.github.com>
2024-12-23 02:26:05 +0300

    Am I stupid check

--- mapper/PolymorphicMapper.hpp
+++ mapper/PolymorphicMapper.hpp
@@ -1,43 +1,59 @@
 #pragma once
 
+#include <iostream>
 #include <optional>
 #include <type_traits>
 
-template <class Source, auto value>
-struct Mapping {
-    using TargetType = std::remove_cv_t<decltype(value)>;
-    static constexpr auto mapped_value = value;
+template <class Source, auto value> struct Mapping {
+  using TargetType = decltype(value);
+  static constexpr auto mapped_value = value;
 };
+
+template <class T> struct TypeWrapper {
+  using type = T;
+};
+
+template <class T> using WrappedType = typename TypeWrapper<T>::type;
 
 template <class Base, class Target, class... MapEntries>
 struct PolymorphicMapper;
 
-template <class Base, class Target>
-struct PolymorphicMapper<Base, Target>
-{
-    static std::optional<Target> map(const Base&) { return std::nullopt; }
+template <class Base, class Target> struct PolymorphicMapper<Base, Target> {
+  static std::optional<Target> map(const Base &) { return std::nullopt; }
 };
 
-template <class Base, class Target, class Derived, auto mappedValue, class... RemainingMappings>
-struct PolymorphicMapper<Base, Target, Mapping<Derived, mappedValue>, RemainingMappings...>
-{
-    static std::optional<Target> map(const Base& instance)
-    {
-        static_assert(std::is_base_of_v<Base, Derived>);
-        if (dynamic_cast<const Derived*>(std::addressof(instance)) != nullptr)
-        {
-            return { mappedValue };
-        }
-        else
-        {
-            if constexpr (sizeof...(RemainingMappings) > 0)
-            {
-                return PolymorphicMapper<Base, Target, RemainingMappings...>::map(instance);
-            }
-            else
-            {
-                return std::nullopt;
-            }
-        }
+template <class Base, class Target, class... MapEntries> struct MapperImpl;
+
+template <class Base, class Target, class Derived, auto mappedValue,
+          class... RemainingMappings>
+  requires std::is_base_of_v<Base, Derived>
+struct MapperImpl<Base, Target, Mapping<Derived, mappedValue>,
+                  RemainingMappings...> {
+  static std::optional<WrappedType<Target>>
+  processMapping(const Base &instance) {
+    if (dynamic_cast<const Derived *>(std::addressof(instance)) != nullptr) {
+      return {mappedValue};
+    } else {
+      return MapperImpl<Base, Target, RemainingMappings...>::processMapping(
+          instance);
     }
+  }
+};
+
+template <class Base, class Target> struct MapperImpl<Base, Target> {
+  static std::optional<WrappedType<Target>>
+  processMapping(const Base &instance) {
+    return PolymorphicMapper<Base, Target>::map(instance);
+  }
+};
+
+template <class Base, class Target, class Derived, auto mappedValue,
+          class... RemainingMappings>
+  requires std::is_base_of_v<Base, Derived>
+struct PolymorphicMapper<Base, Target, Mapping<Derived, mappedValue>,
+                         RemainingMappings...> {
+  static std::optional<Target> map(const Base &instance) {
+    return MapperImpl<Base, Target, Mapping<Derived, mappedValue>,
+                      RemainingMappings...>::processMapping(instance);
+  }
 };
